e0af1b849b9721c3eb8e9dc0adfcbe1438e37a95
BlWoAlf <blwoalf@inbox.ru>
2021-07-05 16:24:13 +0700

    fix

--- src/Controllers/Admin/ProductController.php
+++ src/Controllers/Admin/ProductController.php
@@ -56,6 +56,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $item = Product::findOrFail($id);
         $product = $request->all()['product'];
         
         if(!isset($product['categories'])){
